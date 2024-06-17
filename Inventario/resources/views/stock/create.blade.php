4ec8173617c91723f5d46a58e956281af672805e
ezelpc <164380108+esman1@users.noreply.github.com>
2024-06-17 14:04:33 -0600

    Actualizacion 4.9
    Correccion de Stock y boton de eliminacion

--- Inventario/resources/views/stock/create.blade.php
+++ Inventario/resources/views/stock/create.blade.php
@@ -5,39 +5,124 @@
 @endsection
 
 @section('content')
-    <section class="content container-fluid">
-        <div class="row">
-            <div class="col-md-12">
+<div class="container">
+    <section class="row justify-content-center">
+        <div class="col-md-8"> <!-- Corregido 'cold-md-8' a 'col-md-8' -->
+            <div class="card">
+                <div class="card-header">Ingresar Stock</div> <!-- Corregido 'Igresar Stock' a 'Ingresar Stock' -->
+                <div class="card-body">
+                    <form method="POST" action="{{ route('stock.store') }}" enctype="multipart/form-data">
+                        @csrf
 
-                @includeif('partials.errors')
-
-                <div class="card card-default">
-                <div class='card-header'>
-                    <div style="display: flex; justify-content: space-between; align-items: center;">
-                      
-                        <div class="float-left">
-                            <a class="btn btn-outline-primary btn-sm ml-2" href="javascript:history.back()" title="Volver">
-                                <i class="bi bi-arrow-left-circle"></i>
-                            </a>
+                        <div class="form-group">
+                            {{ Form::label('N.Serie:') }}
+                            {{ Form::text('Nserie', old('Nserie'), ['class' => 'form-control' . ($errors->has('Nserie') ? ' is-invalid' : ''), 'placeholder' => 'Nserie']) }}
+                            {!! $errors->first('Nserie', '<div class="invalid-feedback">:message</div>') !!}
                         </div>
-                        
-                    <strong class="card-title">{{ __('Nuevo') }} Stock</strong>
-                    <div class="float-right">
-                        <a href="{{route ('home')}}" class="btn btn-outline-secondary btn-sm ml-2" title="Panel Principal"><i class="bi bi-house" aria-hidden="true"></i></a> 
-                             
-                    </div>
-                </div>
-                </div>
-                    <div class="card-body">
-                        <form method="POST" action="{{ route('stock.store') }}"  role="form" enctype="multipart/form-data">
-                            @csrf
 
-                           
+                        <div class="form-group">
+                            {{ Form::label('Modelo:') }}
+                            <select class="form-control" name="modelo_id">
+                                <option value="">Selecciona una Opcion ...</option>
+                                @foreach($modelos as $modelo)
+                                    <option value="{{ $modelo->id_modelo }}" {{ old('modelo_id') == $modelo->id_modelo ? 'selected' : '' }}>
+                                        {{ $modelo->nomMod }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
 
-                        </form>
-                    </div>
+                        <div class="form-group">
+                            {{ Form::label('Tipo:') }}
+                            <select class="form-control" name="tipo_id">
+                                <option value="">Selecciona una Opcion ...</option>
+                                @foreach($tipos as $tipo)
+                                    <option value="{{ $tipo->id_tipo }}" {{ old('tipo_id') == $tipo->id_tipo ? 'selected' : '' }}>
+                                        {{ $tipo->nomTipo }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
+
+                        <div class="form-group">
+                            {{ Form::label('Marca:') }}
+                            <select class="form-control" name="marca_id">
+                                <option value="">Selecciona una Opcion ...</option>
+                                @foreach($marcas as $marca)
+                                    <option value="{{ $marca->id_marca }}" {{ old('marca_id') == $marca->id_marca ? 'selected' : '' }}>
+                                        {{ $marca->nomMar }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
+
+                        <div class="form-group">
+                            {{ Form::label('Sistema Operativo:') }}
+                            <select class="form-control" name="sisop_id">
+                                <option value="">Selecciona una Opcion ...</option>
+                                @foreach($sisops as $sisop)
+                                    <option value="{{ $sisop->id_sisop }}" {{ old('sisop_id') == $sisop->id_sisop ? 'selected' : '' }}>
+                                        {{ $sisop->nomSis }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
+
+                        <div class="form-group">
+                            {{ Form::label('Procesador:') }}
+                            <select class="form-control" name="proces_id">
+                                <option value="">Selecciona una Opcion ...</option>
+                                @foreach($proces as $proc)
+                                    <option value="{{ $proc->id_proc }}" {{ old('proces_id') == $proc->id_proc ? 'selected' : '' }}>
+                                        {{ $proc->nomProc }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
+
+                        <div class="form-group">
+                            {{ Form::label('Memoria:') }}
+                            <select class="form-control" name="mem_id">
+                                <option value="">Selecciona una Opcion ...</option>
+                                @foreach($mems as $mem)
+                                    <option value="{{ $mem->id_mem }}" {{ old('mem_id') == $mem->id_mem ? 'selected' : '' }}>
+                                        {{ $mem->tipoMem }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
+
+                        <div class="form-group">
+                            {{ Form::label('Disco Duro:') }}
+                            <select class="form-control" name="disc_d">
+                                <option value="">Selecciona una Opcion ...</option>
+                                @foreach($discs as $disc)
+                                    <option value="{{ $disc->id_disc }}" {{ old('disc_d') == $disc->id_disc ? 'selected' : '' }}>
+                                        {{ $disc->nomDis }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
+
+                        <div class="form-group">
+                            {{ Form::label('Estatus:') }}
+                            <select class="form-control" name="estatus_id">
+                                <option value="">Selecciona una Opcion ...</option>
+                                @foreach($estatus as $est)
+                                    <option value="{{ $est->id_estat }}" {{ old('estatus_id') == $est->id_estat ? 'selected' : '' }}>
+                                        {{ $est->estat }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
+
+                        <div class="box-footer mt20">
+                            <button type="submit" class="btn btn-outline-primary">{{ __('Guardar') }}</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
     </section>
+</div>
 @endsection
